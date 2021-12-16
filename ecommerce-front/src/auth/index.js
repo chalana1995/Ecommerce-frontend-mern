ca8e80ab4e15d699a458d38935130eae4c1170cc
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-12-16 21:38:55 +0530

    signout set

--- ecommerce-front/src/auth/index.js
+++ ecommerce-front/src/auth/index.js
@@ -40,3 +40,20 @@ export const authenticate = (data, next) => {
     next();
   }
 };
+
+export const signout = (next) => {
+  if (typeof Window !== "undefined") {
+    localStorage.removeItem("jwt");
+    next();
+  }
+
+  return fetch(`${API}/signout`, {
+    method: "GET",
+  })
+    .then((response) => {
+      console.log("signout", response);
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
+};
--- ecommerce-front/src/core/Menu.js
+++ ecommerce-front/src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import { signout } from "../auth";
 
 function Menu({ history }) {
   const isActive = (history, path) => {
@@ -37,6 +38,20 @@ function Menu({ history }) {
           >
             SignUp
           </Link>
+        </li>
+
+        <li className="nav-item">
+          <span
+            className="nav-link"
+            style={{ cursor: "pointer", color: "#ffffff" }}
+            onClick={() =>
+              signout(() => {
+                history.push("/");
+              })
+            }
+          >
+            SignOut
+          </span>
         </li>
       </ul>
     </div>
